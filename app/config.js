1bcc7802b74ec36994751f4518c240e3d6bc8b78
spottersolutions <tahaguedouar20@gmail.com>
2026-09-15 19:22:51 +0200

    Use legacy anon key for Supabase auth

--- app/config.js
+++ app/config.js
@@ -1,5 +1,5 @@
 /* Supabase project configuration. */
 window.SUPABASE_CONFIG = {
   url: 'https://agfkymkpyoqhbyjvagki.supabase.co',
-  anonKey: 'sb_publishable_so2yubkDybSHDZo5nzQbYA_4WZabzZp'
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFnZmt5bWtweW9xaGJ5anZhZ2tpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk0ODU2MDYsImV4cCI6MjEwNTA2MTYwNn0.IkwOAPrc2-wMicolpCZM15RHgXQHNHDtPZ5sqz5zQZQ'
 };
