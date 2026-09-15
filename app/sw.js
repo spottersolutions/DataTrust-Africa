44d1a7cf99a6a50c6d95bec960d8af0bdade3b6a
spottersolutions <tahaguedouar20@gmail.com>
2026-09-15 17:58:47 +0200

    Add offline service worker

---
+++ app/sw.js
@@ -0,0 +1,41 @@
+const CACHE = 'datatrust-africa-v1';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './app.jsx',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      const network = fetch(event.request)
+        .then((response) => {
+          if (response.ok && (event.request.url.startsWith(self.location.origin) || event.request.url.includes('unpkg.com') || event.request.url.includes('cdn.tailwindcss.com'))) {
+            const copy = response.clone();
+            caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+          }
+          return response;
+        })
+        .catch(() => cached);
+      return cached || network;
+    })
+  );
+});
